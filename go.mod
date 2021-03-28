a32ed665e18a77571542ed48eb0b6de3a741cf5b
Adam Farden <adam@farden.cz>
2021-03-29 00:47:34 +0200

    Refactor tests to match default Logger

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.16
 
 require (
 	github.com/gofiber/fiber/v2 v2.6.0
-	github.com/gofrs/uuid v4.0.0+incompatible
 	github.com/rs/zerolog v1.21.0
 	github.com/valyala/fasthttp v1.22.0
 )
--- zerologger_test.go
+++ zerologger_test.go
@@ -3,14 +3,18 @@ package zerologger_test
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
+	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
+	"sync"
 	"testing"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/utils"
-	"github.com/gofrs/uuid"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/valyala/fasthttp"
@@ -18,209 +22,324 @@ import (
 	. "czechia.dev/zerologger"
 )
 
-type stdout struct {
-	Time    time.Time `json:"time"`
-	Status  int       `json:"status"`
-	Level   string    `json:"level"`
-	Latency float32   `json:"latency"`
-	IP      string    `json:"ip"`
-	Method  string    `json:"method"`
-	Path    string    `json:"path"`
-	Agent   string    `json:"user-agent"`
-	Message string    `json:"message"`
+type StdOut struct {
+	Pid           string `json:"pid"`
+	Time          string `json:"time"`
+	Referer       string `json:"referer"`
+	Protocol      string `json:"protocol"`
+	IP            string `json:"ip"`
+	IPs           string `json:"ips"`
+	Host          string `json:"host"`
+	Method        string `json:"method"`
+	Path          string `json:"path"`
+	URL           string `json:"url"`
+	UA            string `json:"ua"`
+	Latency       string `json:"latency"`
+	Status        int    `json:"status"`
+	ResBody       string `json:"resBody"`
+	QueryParams   string `json:"queryParams"`
+	Body          string `json:"body"`
+	BytesSent     int    `json:"bytesSent"`
+	BytesReceived int    `json:"bytesReceived"`
+	Route         string `json:"route"`
+	Error         string `json:"error"`
+
+	LocalsDemo string `json:"demo"`
 }
 
-func Test_LoggerStatus(t *testing.T) {
-	type args struct {
-		out    *stdout
-		status int
-	}
-
+func Test_Logger(t *testing.T) {
 	buf := new(bytes.Buffer)
 	log.Logger = zerolog.New(buf)
 
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "StatusContinue",
-			args: args{
-				out:    new(stdout),
-				status: fiber.StatusContinue,
-			},
-		},
-		{
-			name: "StatusOK",
-			args: args{
-				out:    new(stdout),
-				status: fiber.StatusOK,
-			},
-		},
-		{
-			name: "StatusBadRequest",
-			args: args{
-				out:    new(stdout),
-				status: fiber.StatusBadRequest,
-			},
-		},
-		{
-			name: "StatusInternalServerError",
-			args: args{
-				out:    new(stdout),
-				status: fiber.StatusInternalServerError,
-			},
-		},
-	}
+	app := fiber.New()
+	app.Use(New(Config{
+		Format: []string{TagError},
+	}))
 
-	for _, tt := range tests {
-		app := fiber.New()
-		app.Use(New())
-		app.Get("/", func(ctx *fiber.Ctx) error {
-			ctx.WriteString(tt.name)
-			return fiber.NewError(tt.args.status, tt.name)
-		})
+	app.Get("/", func(c *fiber.Ctx) error {
+		return errors.New("some random error")
+	})
 
-		resp, err := app.Test(httptest.NewRequest("GET", "/", nil))
-		utils.AssertEqual(t, nil, err)
-		utils.AssertEqual(t, tt.args.status, resp.StatusCode)
+	resp, err := app.Test(httptest.NewRequest("GET", "/", nil))
+	data, _ := io.ReadAll(buf)
 
-		data, _ := io.ReadAll(buf)
-		json.Unmarshal(data, tt.args.out)
-		utils.AssertEqual(t, StatusMessage[tt.args.status], tt.args.out.Message)
-
-		app.Shutdown()
-	}
+	out := new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusInternalServerError, resp.StatusCode)
+	utils.AssertEqual(t, "some random error", out.Error)
 }
 
-func Test_LoggerConfig(t *testing.T) {
-	type args struct {
-		out    *stdout
-		config []Config
-		status int
-	}
-
+func Test_Logger_locals(t *testing.T) {
 	buf := new(bytes.Buffer)
 	log.Logger = zerolog.New(buf)
 
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "default",
-			args: args{
-				out:    new(stdout),
-				config: []Config{ConfigDefault},
-				status: fiber.StatusOK,
-			},
-		},
-		{
-			name: "false",
-			args: args{
-				out: new(stdout),
-				config: []Config{{
-					Next:         func(ctx *fiber.Ctx) bool { return false },
-					Format:       ConfigDefault.Format,
-					TimeFormat:   ConfigDefault.TimeFormat,
-					TimeZone:     ConfigDefault.TimeZone,
-					TimeInterval: ConfigDefault.TimeInterval,
-				}},
-				status: fiber.StatusOK,
-			},
-		},
-		{
-			name: "true",
-			args: args{
-				out: new(stdout),
-				config: []Config{{
-					Next:         func(ctx *fiber.Ctx) bool { return true },
-					Format:       ConfigDefault.Format,
-					TimeFormat:   ConfigDefault.TimeFormat,
-					TimeZone:     ConfigDefault.TimeZone,
-					TimeInterval: ConfigDefault.TimeInterval,
-				}},
-				status: 0,
-			},
-		},
-		{
-			name: "full",
-			args: args{
-				out: new(stdout),
-				config: []Config{{
-					Format:       []string{TagPid, TagTime, TagReferer, TagProtocol, TagIP, TagIPs, TagHost, TagMethod, TagPath, TagURL, TagUA, TagLatency, TagStatus, TagResBody, TagQueryStringParams, TagBody, TagBytesSent, TagBytesReceived, TagRoute, TagError, "header:x-test", "query:q-test", "form:f-test", "cookie:c-test", "locals:l-bytes", "locals:l-string", "locals:l-uuid"},
-					TimeFormat:   ConfigDefault.TimeFormat,
-					TimeZone:     ConfigDefault.TimeZone,
-					TimeInterval: ConfigDefault.TimeInterval,
-				}},
-				status: fiber.StatusOK,
-			},
-		},
-		{
-			name: "timezone",
-			args: args{
-				out: new(stdout),
-				config: []Config{{
-					Format:       ConfigDefault.Format,
-					TimeFormat:   ConfigDefault.TimeFormat,
-					TimeZone:     "Fake/TimeZone",
-					TimeInterval: ConfigDefault.TimeInterval,
-				}},
-				status: fiber.StatusOK,
-			},
-		},
-	}
+	app := fiber.New()
+	app.Use(New(Config{
+		Format: []string{"locals:demo"},
+	}))
 
-	for _, tt := range tests {
-		app := fiber.New()
-		app.Use(New(tt.args.config...))
-		app.Get("/", func(ctx *fiber.Ctx) error {
-			ctx.Locals("l-bytes", []byte("test"))
-			ctx.Locals("l-string", "test")
-			ctx.Locals("l-uuid", uuid.Nil)
-			ctx.WriteString(tt.name)
-			return fiber.NewError(fiber.StatusOK)
-		})
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.Locals("demo", "johndoe")
+		return c.SendStatus(fiber.StatusOK)
+	})
 
-		req := httptest.NewRequest("GET", "/?q-test=test", nil)
-		req.Header = map[string][]string{
-			"x-test": {"test"},
-		}
+	app.Get("/int", func(c *fiber.Ctx) error {
+		c.Locals("demo", 55)
+		return c.SendStatus(fiber.StatusOK)
+	})
 
-		resp, err := app.Test(req)
-		utils.AssertEqual(t, nil, err)
-		utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
+	app.Get("/bytes", func(c *fiber.Ctx) error {
+		c.Locals("demo", []byte("55"))
+		return c.SendStatus(fiber.StatusOK)
+	})
 
-		data, _ := io.ReadAll(buf)
-		json.Unmarshal(data, tt.args.out)
-		utils.AssertEqual(t, StatusMessage[tt.args.status], tt.args.out.Message)
+	app.Get("/empty", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
 
-		app.Shutdown()
-	}
+	resp, err := app.Test(httptest.NewRequest("GET", "/", nil))
+	data, _ := io.ReadAll(buf)
+
+	out := new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
+	utils.AssertEqual(t, "johndoe", out.LocalsDemo)
+
+	resp, err = app.Test(httptest.NewRequest("GET", "/int", nil))
+	data, _ = io.ReadAll(buf)
+
+	out = new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
+	utils.AssertEqual(t, "55", out.LocalsDemo)
+
+	resp, err = app.Test(httptest.NewRequest("GET", "/bytes", nil))
+	data, _ = io.ReadAll(buf)
+
+	out = new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
+	utils.AssertEqual(t, "55", out.LocalsDemo)
+
+	resp, err = app.Test(httptest.NewRequest("GET", "/empty", nil))
+	data, _ = io.ReadAll(buf)
+
+	out = new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
+	utils.AssertEqual(t, "", out.LocalsDemo)
+}
+
+func Test_Logger_Next(t *testing.T) {
+	app := fiber.New()
+	app.Use(New(Config{
+		Next: func(_ *fiber.Ctx) bool {
+			return true
+		},
+	}))
+
+	resp, err := app.Test(httptest.NewRequest("GET", "/", nil))
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusNotFound, resp.StatusCode)
+}
+
+func Test_Logger_ErrorTimeZone(t *testing.T) {
+	app := fiber.New()
+	app.Use(New(Config{
+		TimeZone: "invalid",
+	}))
+
+	resp, err := app.Test(httptest.NewRequest("GET", "/", nil))
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusNotFound, resp.StatusCode)
+}
+
+func Test_Logger_All(t *testing.T) {
+	buf := new(bytes.Buffer)
+	log.Logger = zerolog.New(buf)
+
+	app := fiber.New()
+	app.Use(New(Config{
+		Format: []string{TagPid, TagTime, TagReferer, TagProtocol, TagIP, TagIPs, TagHost, TagMethod, TagPath, TagURL, TagUA, TagStatus, TagResBody, TagQueryStringParams, TagBody, TagBytesSent, TagBytesReceived, TagRoute, TagError, "header:test", "query:test", "form:test", "cookie:test"},
+	}))
+
+	resp, err := app.Test(httptest.NewRequest("GET", "/?foo=bar", nil))
+	data, _ := io.ReadAll(buf)
+
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusNotFound, resp.StatusCode)
+
+	expected := fmt.Sprintf(`{"level":"warn","pid":"%d","time":"","referer":"","protocol":"http","ip":"0.0.0.0","ips":"","host":"example.com","method":"GET","path":"/","url":"/?foo=bar","ua":"","status":404,"resBody":"Cannot GET /","queryParams":"foo=bar","body":"","bytesSent":12,"bytesReceived":0,"route":"/","test":"","test":"","test":"","test":"","message":"NotFound"}`, os.Getpid())
+	utils.AssertEqual(t, expected, strings.TrimSpace(string(data)))
+}
+
+func Test_Query_Params(t *testing.T) {
+	buf := new(bytes.Buffer)
+	log.Logger = zerolog.New(buf)
+
+	app := fiber.New()
+	app.Use(New(Config{
+		Format: []string{TagQueryStringParams},
+	}))
+
+	resp, err := app.Test(httptest.NewRequest("GET", "/?foo=bar&baz=moz", nil))
+	data, _ := io.ReadAll(buf)
+
+	out := new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusNotFound, resp.StatusCode)
+
+	expected := "foo=bar&baz=moz"
+	utils.AssertEqual(t, expected, out.QueryParams)
+}
+
+func Test_Response_Body(t *testing.T) {
+	buf := new(bytes.Buffer)
+	log.Logger = zerolog.New(buf)
+
+	app := fiber.New()
+	app.Use(New(Config{
+		Format: []string{TagResBody},
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Sample response body")
+	})
+
+	app.Post("/test", func(c *fiber.Ctx) error {
+		return c.Send([]byte("Post in test"))
+	})
+
+	_, err := app.Test(httptest.NewRequest("GET", "/", nil))
+	data, _ := io.ReadAll(buf)
+
+	out := new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+
+	expectedGetResponse := "Sample response body"
+	utils.AssertEqual(t, expectedGetResponse, out.ResBody)
+
+	_, err = app.Test(httptest.NewRequest("POST", "/test", nil))
+	data, _ = io.ReadAll(buf)
+
+	out = new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+
+	expectedPostResponse := "Post in test"
+	utils.AssertEqual(t, expectedPostResponse, out.ResBody)
+}
+
+func Test_Logger_AppendUint(t *testing.T) {
+	buf := new(bytes.Buffer)
+	log.Logger = zerolog.New(buf)
+
+	app := fiber.New()
+	app.Use(New(Config{
+		Format: []string{
+			TagBytesReceived, TagBytesSent, TagStatus,
+		},
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello")
+	})
+
+	resp, err := app.Test(httptest.NewRequest("GET", "/", nil))
+	data, _ := io.ReadAll(buf)
+
+	out := new(StdOut)
+	json.Unmarshal(data, out)
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusOK, resp.StatusCode)
+	utils.AssertEqual(t, 0, out.BytesReceived)
+	utils.AssertEqual(t, 5, out.BytesSent)
+	utils.AssertEqual(t, 200, out.Status)
+}
+
+func Test_Logger_Data_Race(t *testing.T) {
+	buf := new(bytes.Buffer)
+	log.Logger = zerolog.New(buf)
+
+	app := fiber.New()
+	app.Use(New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello")
+	})
+
+	var (
+		resp1, resp2 *http.Response
+		err1, err2   error
+	)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		resp1, err1 = app.Test(httptest.NewRequest("GET", "/", nil))
+		wg.Done()
+	}()
+	resp2, err2 = app.Test(httptest.NewRequest("GET", "/", nil))
+	wg.Wait()
+
+	utils.AssertEqual(t, nil, err1)
+	utils.AssertEqual(t, fiber.StatusOK, resp1.StatusCode)
+	utils.AssertEqual(t, nil, err2)
+	utils.AssertEqual(t, fiber.StatusOK, resp2.StatusCode)
+}
+
+func Test_Logger_Redirect(t *testing.T) {
+	buf := new(bytes.Buffer)
+	log.Logger = zerolog.New(buf)
+
+	app := fiber.New()
+	app.Use(New(Config{
+		Format: []string{
+			TagBytesReceived, TagBytesSent, TagStatus,
+		},
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Redirect("/", fiber.StatusContinue)
+	})
+
+	resp, err := app.Test(httptest.NewRequest("GET", "/", nil))
+	utils.AssertEqual(t, nil, err)
+	utils.AssertEqual(t, fiber.StatusContinue, resp.StatusCode)
 }
 
 func Benchmark_Logger(b *testing.B) {
 	log.Logger = zerolog.New(io.Discard)
 
 	app := fiber.New()
-	app.Use(New())
 
+	app.Use(New(Config{
+		Format: []string{
+			TagBytesReceived, TagBytesSent, TagStatus,
+		},
+	}))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
 
 	h := app.Handler()
 
-	ctx := &fasthttp.RequestCtx{}
-	ctx.Request.Header.SetMethod("GET")
-	ctx.Request.SetRequestURI("/")
+	fctx := &fasthttp.RequestCtx{}
+	fctx.Request.Header.SetMethod("GET")
+	fctx.Request.SetRequestURI("/")
 
 	b.ReportAllocs()
 	b.ResetTimer()
 
 	for n := 0; n < b.N; n++ {
-		h(ctx)
+		h(fctx)
 	}
 
-	utils.AssertEqual(b, 200, ctx.Response.Header.StatusCode())
+	utils.AssertEqual(b, 200, fctx.Response.Header.StatusCode())
 }
